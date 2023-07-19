1a09b6eaada9085b883915e776fef08e9551924d
Prathiba <prathiba.jeevan.external@telefonica.com>
2023-07-19 07:18:09 +0200

    To implement the Front End page for Representation Layer /v1/represent-flow-visualization-page Fixes #21

--- impl/client/src/App.js
+++ impl/client/src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { AdvancedForm } from './components/forms/AdvancedForm'
+import { FlowDiagram } from './components/forms/Flow/FlowDiagram'
+import {Buffer} from 'buffer';
+import axios from 'axios';
+import randExp from 'randexp';
 
-function App() {
+export default function App() {
+  const [flowValues, setFlowValues] = useState([])
+  
+  const handleSubmit = async (values, { setSubmitting }) => {
+    console.log(values);
+    let xCorrelator = values.xCorrelator;
+    let userName = values.username;
+    let password = values.password;
+    let basicAuth = "Basic " + Buffer.from(userName + ":" + password).toString('base64');   
+
+    setSubmitting(true)
+
+    getListOfRecords(xCorrelator, basicAuth).then((recordList) => {
+      setFlowValues(recordList);
+    });
+    
+    await new Promise((r) => setTimeout(r, 1000))
+    setSubmitting(false)
+  }
+
+  const numberOfRecordsArray = [];
+  for(let i=0;i<10000;i++){
+    let numberOfRecords =  { label: i, value: i };
+    numberOfRecordsArray.push(numberOfRecords);
+  }
+
+  const formSchema = [
+    { name: 'xCorrelator', label: 'x-correlator', componentType: 'text', required: true },
+    { name: 'username', label: 'Username', componentType: 'text', required: true },
+    { name: 'password', label: 'Password', componentType: 'password', required: true },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Flow Representation</h1>
+      <div className="flex">
+        <div className="form section">
+          <AdvancedForm schema={formSchema} onSubmit={handleSubmit} />
+        </div>
+      </div>
+      <br/>
+      <FlowDiagram input={flowValues}/>
+    </>
+  )
+
+  async function getListOfRecords(xCorrelator, basicAuth) {
+    let listOfRecords = [];
+    try {
+        let requestHeader = {
+            'accept': 'application/json',
+            'user': 'RequestSequenceRepresentation',
+            'originator': 'RequestSequenceRepresentation',
+            'x-correlator': getRandomXCorrelator(),
+            'trace-indicator': '1',
+            'customer-journey': 'Unknown value',
+            'Authorization': basicAuth,
+            'Content-Type': 'application/json'
+        }
+    
+        let requestBody = {
+            "x-correlator": xCorrelator
+        };
+        let request = {
+            method: "post",
+            url: "http://localhost:3015/v1/update-flow-visualization-page",
+            headers: requestHeader,
+            data: requestBody
+        }
+        listOfRecords = (await axios(request)).data;
+    } catch (error) {
+        console.log(error);
+    }    
+    return listOfRecords;
 }
 
-export default App;
+function getRandomXCorrelator() {
+    let randomXCorrelatorString;
+    try {
+        randomXCorrelatorString = new randExp(/^[0-9A-Fa-f]{8}(?:-[0-9A-Fa-f]{4}){3}-[0-9A-Fa-f]{12}$/).gen();
+    } catch (error) {
+        console.log("error");
+        console.log(error);
+    }
+    return randomXCorrelatorString;
+}
+}
