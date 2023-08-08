0591c7152a97f430807032cd0d5138fd49e1e342
sunil-kumar-meena <sunil@Sunils-MacBook-Air.local>
2023-08-08 20:39:18 +0530

    modified accordion tab

--- impl/client/src/components/Accordion/Accordion.js
+++ impl/client/src/components/Accordion/Accordion.js
@@ -1,17 +1,32 @@
 import React, { useState } from 'react';
 import './Accordion.css';
 
+/**
+ * below funtion handles acordion tab's open and close functionality
+ */
+const accordionTab = (element) => {
+         element.target.nextElementSibling.classList.toggle("inactive");
+         element.target.classList.toggle("open");
+         var accordionPanel = element.target.nextElementSibling;
+         if (accordionPanel.style.maxHeight) {
+            accordionPanel.style.maxHeight = null;
+         } else {
+            accordionPanel.style.maxHeight = accordionPanel.scrollHeight + "px";
+         }
+};
+
+/**
+ * Accordion component to create accordion's heading and body
+ */
 function Accordion({ AccordionContent }) {
-   const [activeIndex, setActiveIndex] = useState(-1);
-   const handleClick = (index) => {
-      setActiveIndex(index === activeIndex ? -1 : index);
-   };
    return (
       <div>
-         {AccordionContent.map((accordion, index) => (
+         {AccordionContent.map((accordion) => (
             <div key={accordion.title}>
-               <h2 className='form-title' onClick={() => handleClick(index)}>{accordion.title}</h2>
-               {index === activeIndex && accordion.content}
+               <h2 className='form-title accordion close' onClick={accordionTab} >{accordion.title}</h2>
+               <div class="panel">
+                  { (accordion.content) }
+               </div>
             </div>
          ))}
       </div>
